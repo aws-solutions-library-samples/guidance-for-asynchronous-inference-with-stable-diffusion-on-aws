1066f1b0f4cdb08d036bf7605495a3daff275d28
yubingjiaocn <yubingjiaocn@gmail.com>
2024-09-10 03:32:15 +0000

    Replace default SD 1.5 model to SD 2.1 due to takedown

--- deploy/upload-model.sh
+++ deploy/upload-model.sh
@@ -6,10 +6,10 @@ MODEL_BUCKET="$1"
 
 AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')}
 
-MODEL_URL="https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors?download=true"
-MODEL_NAME="v1-5-pruned-emaonly.safetensors"
+MODEL_URL="https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.safetensors"
+MODEL_NAME="v2-1_768-ema-pruned.safetensors"
 
-printf "Transport SD 1.5 base model from hugging face to S3 bucket...\n"
+printf "Transport SD 2.1 base model from hugging face to S3 bucket...\n"
 curl -L "$MODEL_URL" | aws s3 cp - s3://${MODEL_BUCKET}/Stable-diffusion/${MODEL_NAME}
 
 printf "Model uploaded to s3://${MODEL_BUCKET}/Stable-diffusion/${MODEL_NAME}\n"
